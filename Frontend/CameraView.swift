c946227ee0954cdedf952a200ff6b0b26c7a9213
AdaliaWilliams <97926269+AdaliaWilliams@users.noreply.github.com>
2023-11-14 10:18:20 -0500

    Create CameraView.swift
    adding CameraView.swift to frontEnd

---
+++ Frontend/CameraView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct CameraView: View {
+    
+    @State private var image: Image? = Image("karthick")
+    @State private var shouldPresentImagePicker = false
+    @State private var shouldPresentActionScheet = false
+    @State private var shouldPresentCamera = false
+    
+    var body: some View {
+        // WARNING: Force wrapped image for demo purpose
+        image!
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 300, height: 300)
+            .frame(width: 200, height: 200, alignment: .center)
+            .clipped()
+            .shadow(radius: 10)
+            .onTapGesture { self.shouldPresentActionScheet = true }
+            .sheet(isPresented: $shouldPresentImagePicker) {
+                SUImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
+        }.actionSheet(isPresented: $shouldPresentActionScheet) { () -> ActionSheet in
+            ActionSheet(title: Text("Choose mode"), message: Text("Please choose your preferred mode to set your profile image"), buttons: [ActionSheet.Button.default(Text("Camera"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = true
+            }), ActionSheet.Button.default(Text("Photo Library"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = false
+            }), ActionSheet.Button.cancel()])
+        }
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
